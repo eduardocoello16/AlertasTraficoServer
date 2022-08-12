83151ba773615b9cbcea887f02ed76684be40cc2
EduardoKDE <eduardocoello16@gmail.com>
2022-08-12 10:47:28 +0100

    Refactorizado y ordenado de código

--- bot.js
+++ bot.js
@@ -1,162 +1,25 @@
-const { Telegraf } = require('telegraf')
-require('dotenv').config({path: './.env'});
+const {
+    Telegraf
+} = require('telegraf')
+require('dotenv').config({
+    path: './.env'
+});
 const fs = require('fs');
 const axios = require('axios').default;
-const  twitter  = require('./twitter')
+const twitter = require('./twitter')
 //Variables usuarios
 const usuariosAdmin = JSON.parse(process.env.BOT_AdminUsers)
+const grupoAdmins = process.env.BOT_AdminGroup
+const grupoAlertas = process.env.BOT_GroupToSend
+const canalAlertas = process.env.BOT_ChannelToSend
 
 //Creando el bot
 const bot = new Telegraf(process.env.BOT_TOKEN_Beta)
 //Comprobar si el archivo bot.log existe, si no crearlo
-if(fs.existsSync('./bot.log') === false){
+if (fs.existsSync('./bot.log') === false) {
     fs.writeFileSync('./bot.log', 'Registro de tweets enviados al canal\n')
-    
-}
-//Cambiar permisos un usuario a administrador y hacerlo anónimo
-bot.command('modoOculto', async (ctx) => {
-    
-    let id = ctx.message.from.id
-    //Comprobar que un usuario es anonimo
-    let user = await bot.telegram.getChatMember(process.env.BOT_GroupToSend, id)
-
-    console.log(user.can_change_info)
-    try {
-        if(user.is_anonymous === true){
-        await bot.telegram.promoteChatMember(process.env.BOT_GroupToSend, id, {is_anonymous: false, can_change_info: user.can_change_info, can_delete_messages: user.can_delete_messages, can_invite_users: user.can_invite_users, can_restrict_members: user.can_restrict_members, can_pin_messages: user.can_pin_messages, can_promote_members: user.can_promote_members})
-        ctx.reply(`${ctx.message.from.first_name} ha desactivado el modo oculto `)
-    }else{
-        await bot.telegram.promoteChatMember(process.env.BOT_GroupToSend, id, {is_anonymous: true, can_change_info: user.can_change_info, can_delete_messages: user.can_delete_messages, can_invite_users: user.can_invite_users, can_restrict_members: user.can_restrict_members, can_pin_messages: user.can_pin_messages, can_promote_members: user.can_promote_members})
-        ctx.reply(`${ctx.message.from.first_name} ha activado el modo oculto `)
-    }
-        
-        
-    } catch (error) {
-        ctx.reply('Ha ocurrido un error al cambiar el modo oculto. ¿Eres un administrador asignado por el bot?')
-    }
-
-})
-
-//Hacer un usuario administrador
-bot.command('setAdmin', async (ctx) => {
-    if(comprobarAdmin(ctx) === true){
-        console.log(ctx.message.text)
-        //Obtener usuario a añadir a la lista de administradores
-        let id = ctx.message.text.split(' ')[1]
-            //Comprobar que un usuario es anonimo
-            try {
-                let user = await bot.telegram.getChatMember(process.env.BOT_GroupToSend, id)
-    if(user.status === 'left'){
-        ctx.reply('El usuario no está en el grupo')
-    }else{
-        //Añadir usuario a la lista de administradores
-       try {
-        await bot.telegram.promoteChatMember(process.env.BOT_GroupToSend, id, { can_change_info: true, can_delete_messages: true,can_manage_chat: true ,can_invite_users: true, can_restrict_members: true, can_pin_messages: true, can_manage_video_chats: true, can_promote_members: true}) 
-        ctx.reply(`Se han actualizado los permisos del usuario ${user.user.first_name} ${user.user.last_name}`)
-       } catch (error) {
-        ctx.reply('Error al añadir usuario')
-       }
-    }
-        } catch (error) {
-        ctx.reply('Id o nombre inválido')
-        }
-    }else{
-        ctx.reply('No tienes permisos para ejecutar este comando')
-    }
-})
-
-//Quitar un usuario administrador
-bot.command('delAdmin', async (ctx) => {
-    if(comprobarAdmin(ctx) === true){
-        console.log(ctx.message.text)
-        //Obtener usuario a añadir a la lista de administradores
-        let id = ctx.message.text.split(' ')[1]
-            //Comprobar que un usuario es anonimo
-            try {
-                let user = await bot.telegram.getChatMember(process.env.BOT_GroupToSend, id)
-    if(user.status === 'left'){
-        ctx.reply('El usuario no está en el grupo')
-    }else{
-        //Añadir usuario a la lista de administradores
-       try {
-        await bot.telegram.promoteChatMember(process.env.BOT_GroupToSend, id, { }) 
-        ctx.reply(`Se han actualizado los permisos del usuario ${user.user.first_name} ${user.user.last_name}`)
-       } catch (error) {
-        ctx.reply('Error al añadir usuario')
-       }
-    }
-        } catch (error) {
-        ctx.reply('Id o nombre inválido')
-        }
-    }else{
-        ctx.reply('No tienes permisos para ejecutar este comando')
-    }
-})
-
-//Obtener el ID de un usuario
-bot.command('getId' , async (ctx) => {
-let id;
-if(ctx.update.message.chat.type === 'private'){
-id = ctx.message.from.id
-}else{
-     id = ctx.update.message.from.id
-}
-ctx.reply(id)
-})
-
-
-bot.start((ctx) => {
-    //Get group id
-    var id = ctx.update.message.chat;
-    console.log(id)
-    ctx.reply('Welcome')
-}
-)
-//Comando /getBlackList -> Obtiene la BlackList del JSON 
-bot.command('getBlackList', (ctx) => {
-    if(comprobarAdmin(ctx) === true){
-    let rawdata = fs.readFileSync('filtro.json');
-    let result = JSON.parse(rawdata);
-    let blackList = result.blackList;
-    ctx.reply(`BlackList:\n${blackList}`)
-    }else{
-        ctx.reply('No tienes permisos para ejecutar este comando')
-    }
 
 }
-)
-//Comando /getBlackListGroup -> Obtiene la BlackListGroup del JSON 
-bot.command('getBlackListGroup', (ctx) => {
-    if(comprobarAdmin(ctx) === true){
-    let rawdata = fs.readFileSync('filtro.json');
-    let result = JSON.parse(rawdata);
-    let blackList = result.blackListGroup;
-    ctx.reply(`BlackList para filtrar en el grupo:\n${blackList}`)
-    }
-    else{
-        ctx.reply('No tienes permisos para ejecutar este comando')
-    }
-    
-}
-)
-
-//Comando /getWhiteList -> Obtiene la WhiteList del JSON 
-bot.command('getWhiteList', (ctx) => {
-    //Comprobar el usuario es admin
-    if(comprobarAdmin(ctx) === true){
-
-    let rawdata = fs.readFileSync('filtro.json');
-    let result = JSON.parse(rawdata);
-    let whiteList = result.whiteList;
-    ctx.reply(`WhiteList:\n${whiteList}`)
-}
-else{
-    ctx.reply('No tienes permisos para ejecutar este comando')
-}
-    
-}
-)
-
 //Start bot
 //Detectar cuando el bot se conecta
 console.log('Iniciando bot... ')
@@ -166,17 +29,192 @@ bot.launch().then(() => {
     setInterval(() => {
         console.log("Comprobando Tweets cada 10 minutos");
         comprobarTweets();
-    },300000);
-}
-).catch(err => {
+    }, 300000);
+}).catch(err => {
     console.log(err)
+})
+
+// COMANDOS
+
+// Iniciar el bot Bienvenido 
+bot.start((ctx) => {
+    //Get group id
+    var id = ctx.update.message.chat;
+    console.log(id)
+    ctx.reply('Hola, bienvenído al BOT  de Alertas de Tráfico TNF')
+})
+
+//Obtener el ID de un usuario
+bot.command('getId', async (ctx) => {
+    let id;
+    if (ctx.update.message.chat.type === 'private') {
+        id = ctx.message.from.id
+    } else {
+        id = ctx.update.message.from.id
+    }
+    ctx.reply(id)
+})
+
+//Cambiar permisos un usuario a administrador y hacerlo anónimo
+bot.command('modoOculto', async (ctx) => {
+    if (ctx.message.chat.id == grupoAdmins) {
+        let id = ctx.message.from.id
+        //Comprobar que un usuario es anonimo
+        let user = await bot.telegram.getChatMember(grupoAlertas, id)
+        try {
+            if (user.is_anonymous === true) {
+                await bot.telegram.promoteChatMember(grupoAlertas, id, {
+                    is_anonymous: false,
+                    can_change_info: user.can_change_info,
+                    can_delete_messages: user.can_delete_messages,
+                    can_invite_users: user.can_invite_users,
+                    can_restrict_members: user.can_restrict_members,
+                    can_pin_messages: user.can_pin_messages,
+                    can_promote_members: user.can_promote_members
+                })
+                ctx.reply(`${ctx.message.from.first_name} ha desactivado el modo oculto `)
+            } else {
+                await bot.telegram.promoteChatMember(grupoAlertas, id, {
+                    is_anonymous: true,
+                    can_change_info: user.can_change_info,
+                    can_delete_messages: user.can_delete_messages,
+                    can_invite_users: user.can_invite_users,
+                    can_restrict_members: user.can_restrict_members,
+                    can_pin_messages: user.can_pin_messages,
+                    can_promote_members: user.can_promote_members
+                })
+                ctx.reply(`${ctx.message.from.first_name} ha activado el modo oculto `)
+            }
+
+
+        } catch (error) {
+            ctx.reply('Ha ocurrido un error al cambiar el modo oculto. ¿Eres un administrador asignado por el bot?')
+        }
+    } else {
+        ctx.reply('Comando solo para administradores')
+    }
+})
+
+//Hacer un usuario administrador
+bot.command('setAdmin', async (ctx) => {
+    if (comprobarAdmin(ctx) === true) {
+        console.log(ctx.message.text)
+        //Obtener usuario a añadir a la lista de administradores
+        let id = ctx.message.text.split(' ')[1]
+        //Comprobar que un usuario es anonimo
+        try {
+            let user = await bot.telegram.getChatMember(grupoAlertas, id)
+            if (user.status === 'left') {
+                ctx.reply('El usuario no está en el grupo')
+            } else {
+                //Añadir usuario a la lista de administradores
+                try {
+                    await bot.telegram.promoteChatMember(grupoAlertas, id, {
+                        can_change_info: true,
+                        can_delete_messages: true,
+                        can_manage_chat: true,
+                        can_invite_users: true,
+                        can_restrict_members: true,
+                        can_pin_messages: true,
+                        can_manage_video_chats: true,
+                        can_promote_members: true
+                    })
+                    ctx.reply(`Se han actualizado los permisos del usuario ${user.user.first_name} ${user.user.last_name}`)
+                } catch (error) {
+                    ctx.reply('Error al añadir usuario')
+                }
+            }
+        } catch (error) {
+            ctx.reply('Id o nombre inválido')
+        }
+    } else {
+        ctx.reply('No tienes permisos para ejecutar este comando')
+    }
+})
+
+//Quitar un usuario administrador
+bot.command('delAdmin', async (ctx) => {
+    if (comprobarAdmin(ctx) === true) {
+        console.log(ctx.message.text)
+        //Obtener usuario a añadir a la lista de administradores
+        let id = ctx.message.text.split(' ')[1]
+        //Comprobar que un usuario es anonimo
+        try {
+            let user = await bot.telegram.getChatMember(grupoAlertas, id)
+            if (user.status === 'left') {
+                ctx.reply('El usuario no está en el grupo')
+            } else {
+                //Añadir usuario a la lista de administradores
+                try {
+                    await bot.telegram.promoteChatMember(grupoAlertas, id, {})
+                    ctx.reply(`Se han actualizado los permisos del usuario ${user.user.first_name} ${user.user.last_name}`)
+                } catch (error) {
+                    ctx.reply('Error al añadir usuario')
+                }
+            }
+        } catch (error) {
+            ctx.reply('Id o nombre inválido')
+        }
+    } else {
+        ctx.reply('No tienes permisos para ejecutar este comando')
+    }
+})
+//Comando /getBlackList -> Obtiene la BlackList del JSON 
+bot.command('getBlackList', (ctx) => {
+    let filtrado = obtenerFiltro('getBlackList', ctx)
+    if (filtrado) {
+        ctx.reply(`Black List:\n${filtrado}`)
+    } else {
+        ctx.reply('No tienes permisos para ejecutar este comando')
+    }
+})
+//Comando /getBlackListGroup -> Obtiene la BlackListGroup del JSON 
+bot.command('getBlackGroupList', (ctx) => {
+    let filtrado = obtenerFiltro('getBlackGroupList', ctx)
+    if (filtrado) {
+        ctx.reply(`Black List Grupo:\n${filtrado}`)
+    } else {
+        ctx.reply('No tienes permisos para ejecutar este comando')
+    }
+})
+//Comando /getWhiteList -> Obtiene la WhiteList del JSON 
+bot.command('getWhiteList', (ctx) => {
+    let filtrado = obtenerFiltro('getWhiteList', ctx)
+    if (filtrado) {
+        ctx.reply(`WhiteList:\n${filtrado}`)
+    } else {
+        ctx.reply('No tienes permisos para ejecutar este comando')
+    }
+})
+
+function obtenerFiltro(filtro, ctx) {
+    //Comprobar el usuario es admin
+    if (comprobarAdmin(ctx) === true) {
+
+        let rawdata = fs.readFileSync('filtro.json');
+        let result = JSON.parse(rawdata);
+        let salida
+        //Switch 
+        switch (filtro) {
+            case 'getWhiteList':
+                salida = result.whiteList
+                break;
+            case 'getBlackList':
+                salida = result.blackList
+            case 'getBlackGroupList':
+                salida = result.blackListGroup
+
+            default:
+                break;
+        }
+        return salida
+    }
 }
-)
 
+//Funciónes Obtener tweets
 
-
-
-function comprobarTweets(){
+//Comprobar tweets nuevos
+function comprobarTweets() {
     console.log('Comprobación de tweets nuevos')
     var cuentasTwitter = JSON.parse(process.env.Twitter_Accounts);
     cuentasTwitter.forEach(cuenta => {
@@ -186,84 +224,76 @@ function comprobarTweets(){
 
 }
 
- 
+//Obtener tweets llamando a la API twitter
 async function obtenerTweets(id, name) {
-   
+
     var tweets = await twitter.getTwett(id)
-    for(const tweet of tweets){
+    for (const tweet of tweets) {
         //Await 3 segundos para no saturar el BOT
         await new Promise(r => setTimeout(r, 3000));
-          //Comprobar si el tweet ya se ha guardado en los logs (Si ha sido enviado o descartado anteriormente)
-    if(comprobarLog(tweet,name) === false){
-        //Filtrar Tweet
-        if( filtradoAcceso(tweet) === true){
-            if(filtradoBlackList(tweet) === true){
-                
-                  
-                       
-                        try {
-                            enviarMensaje(tweet, name, process.env.BOT_GroupToSend);
-                        } catch (error) {
-                            console.log(error)
-                        }
-               
-                
-            }else{
-                 try {
-                    enviarMensaje(tweet, name, process.env.BOT_ChannelToSend);
-                 } catch (error) {
-                    console.log(error)
-                 }
-                 
-                     
-              
-                    
+        //Comprobar si el tweet ya se ha guardado en los logs (Si ha sido enviado o descartado anteriormente)
+        if (comprobarLog(tweet, name) === false) {
+            //Filtrar Tweet
+            if (filtradoAcceso(tweet) === true) {
+                if (filtradoBlackList(tweet) === true) {
+
+
+
+                    try {
+                        enviarMensaje(tweet, name, grupoAlertas);
+                    } catch (error) {
+                        console.log(error)
+                    }
+                } else {
+                    try {
+                        enviarMensaje(tweet, name, canalAlertas);
+                    } catch (error) {
+                        console.log(error)
+                    }
+                }
+
             }
-           
-        }
-        
-    }else{
+
+        } else {
             //Obtener hora y fecha actual 
             let fecha = new Date()
             let hora = fecha.getHours() + ':' + fecha.getMinutes() + ':' + fecha.getSeconds()
-        //Guardar en .log fecha y hora del tweet
-        console.log(`Mensaje con ID:  ${tweet.id} ya envíado. Cuenta:${name}  [${hora}]`)
-     
+            //Guardar en .log fecha y hora del tweet
+            console.log(`Mensaje con ID:  ${tweet.id} ya envíado. Cuenta:${name}  [${hora}]`)
+
+        }
     }
-}
-   
-}
-
-function enviarMensaje(tweet,name, destinatario){
- //Enviar tweet al grupo
- try {
-    bot.telegram.sendMessage(destinatario,`${tweet.text}\n${name}`,
-    //Send message without url preview
-    {disable_web_page_preview: true})
- } catch (error) {
-     console.log(error)
- }
-    
-
-
- //Mensaje sin sonido = disable_notification: true
 
 }
 
-function comprobarLog(tweet, name){
-        
+function enviarMensaje(tweet, name, destinatario) {
+    //Enviar tweet al grupo
+    try {
+        bot.telegram.sendMessage(destinatario, `${tweet.text}\n${name}`,
+            //Send message without url preview
+            {
+                disable_web_page_preview: true
+            })
+    } catch (error) {
+        console.log(error)
+    }
+    //Mensaje sin sonido = disable_notification: true
+}
+
+function comprobarLog(tweet, name) {
+
     //Comprobar un registro .log si el tweet se ha enviado
     var log = fs.readFileSync('./bot.log', 'utf8')
-    if(log.includes(tweet.id)){
+    if (log.includes(tweet.id)) {
         return true
-    }else{
+    } else {
         //Añade el id del  tweet al .log
-        fs.appendFileSync('./bot.log', `Id Tweet: ${tweet.id}\nUsuario: ${name}`+ `\n${new Date()}\n`)
+        fs.appendFileSync('./bot.log', `Id Tweet: ${tweet.id}\nUsuario: ${name}` + `\n${new Date()}\n`)
         return false
     }
 }
 
- function filtradoAcceso(tweet){
+function filtradoAcceso(tweet) {
     let salida = false
     let rawdata = fs.readFileSync('filtro.json');
     let result = JSON.parse(rawdata);
@@ -271,27 +301,28 @@ function comprobarLog(tweet, name){
     let blackList = result.blackList;
     let tweetText = tweet.text.toLowerCase();
     whiteList.forEach(element => {
-        if(tweetText.includes(element.toLowerCase())){
-       
+        if (tweetText.includes(element.toLowerCase())) {
+
             salida = true
         }
     })
     blackList.forEach(element => {
-        if(tweetText.includes(element.toLowerCase())){
+        if (tweetText.includes(element.toLowerCase())) {
             salida = false
         }
-    })   
+    })
     return salida
 }
-function filtradoBlackList(tweet){
+
+function filtradoBlackList(tweet) {
     let salida = false
     let rawdata = fs.readFileSync('filtro.json');
     let result = JSON.parse(rawdata);
     let whiteList = result.blackListGroup;
     let tweetText = tweet.text.toLowerCase();
     whiteList.forEach(element => {
-        if(tweetText.includes(element.toLowerCase())){
-       
+        if (tweetText.includes(element.toLowerCase())) {
+
             salida = true
         }
     })
@@ -302,18 +333,18 @@ function comprobarAdmin(ctx) {
     console.log('Comprobando permisos de administrador')
     let salida = false
     let id;
-    if(ctx.update.message.chat.type === 'private'){
-    id = ctx.message.from.id
-    }else{
-         id = ctx.update.message.from.id
-    }
-   
-    if(id === null){
+    if (ctx.update.message.chat.type === 'private') {
+        id = ctx.message.from.id
+    } else {
         id = ctx.update.message.from.id
     }
-    if(usuariosAdmin.includes(id)){
+
+    if (id === null) {
+        id = ctx.update.message.from.id
+    }
+    if (usuariosAdmin.includes(id)) {
         salida = true
     }
 
     return salida
-  }
+}
--- twitter.js
+++ twitter.js
@@ -1,23 +1,26 @@
+require('dotenv').config({
+  path: './.env'
+});
+async function getTwett(id) {
+  const axios = require('axios').default;
 
-  require('dotenv').config({path: './.env'});
-  async function getTwett(id){
-    const axios = require('axios').default;
+  var url = `https://api.twitter.com/2/users/${id}/tweets`;
+  var token = process.env.Twitter_token;
 
-      var url = `https://api.twitter.com/2/users/${id}/tweets`;
-        var token =  process.env.Twitter_token;
-       
-        var config = {
-          headers: { Authorization: `Bearer ${token}` }
-          
-        }
-  let result = await  axios.get(url, config)
-    
-       return result.data.data.reverse();
-      
+  var config = {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+
   }
+  let result = await axios.get(url, config)
+
+  return result.data.data.reverse();
+
+}
 
 
 
-  module.exports = {
-    getTwett
-  };
+module.exports = {
+  getTwett
+};
