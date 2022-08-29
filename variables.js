c74c74d06811e6ae4d17e081b5c3535f3da9c773
Eduardo <30601020+eduardocoello16@users.noreply.github.com>
2022-08-30 00:35:37 +0100

    variables para main

--- variables.js
+++ variables.js
@@ -1,18 +1,13 @@
 const usuariosAdmin = JSON.parse(process.env.BOT_AdminUsers)
-const grupoAdmins = process.env.BOT_AdminGroup_Test
-const grupoAlertas = process.env.BOT_GroupToSend_Test
-const canalAlertas = process.env.BOT_ChannelToSend_Test
+const grupoAdmins = process.env.BOT_AdminGroup
+const grupoAlertas = process.env.BOT_GroupToSend
+const canalAlertas = process.env.BOT_ChannelToSend
 //Bot
 const {
     Telegraf
 } = require('telegraf')
-const botToken = process.env.BOT_TOKEN_Test
-const bot = new Telegraf(botToken, 
-    {
-        telegram: {
-           testEnv: true
-        } 
-        })
+const botToken = process.env.BOT_TOKEN
+const bot = new Telegraf(botToken)
 
 module.exports = {
     usuariosAdmin,
