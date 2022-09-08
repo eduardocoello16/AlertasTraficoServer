fc28b084e83e165703e3a5f7092b0a42ba09919f
Eduardo <30601020+eduardocoello16@users.noreply.github.com>
2022-09-08 01:28:54 +0100

    uri

--- variables.js
+++ variables.js
@@ -2,6 +2,7 @@ const usuariosAdmin = JSON.parse(process.env.BOT_AdminUsers)
 const grupoAdmins = process.env.BOT_AdminGroup
 const grupoAlertas = process.env.BOT_GroupToSend
 const canalAlertas = process.env.BOT_ChannelToSend
+const mongoDbUri = process.env.MongoDbUri
 //Bot
 const {
     Telegraf
