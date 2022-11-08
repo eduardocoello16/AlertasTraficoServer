155064d94ece04cd5282cdff01ed9d6770d2ad1d
eduardocoello16 <eduardocoello16@gmail.com>
2022-11-08 12:24:41 +0000

    cambios

--- accionesBot/inlineActions.js
+++ accionesBot/inlineActions.js
@@ -7,14 +7,20 @@ import { Markup } from 'telegraf';
 import { nuevoMensaje} from '../webBot/alertasUsuario.js';
 function inlineCommands(bot,database){
 	
-
+function control(ctx){
+	let salida = false;
+salida = ((ctx.update.inline_query.chat_type === 'supergroup') ? true : false);
+salida = ((ctx.update.inline_query.query.length >  5) ? true : false);
+salida = ((ctx.update.inline_query.query.includes(' ')  ) ? true : false);
+return salida
+}
  
 	bot.on('inline_query', async (ctx) => {
 	
 		let user = ctx.from;
 		let idUsuario = ctx.from.id;
 		const getUsuario = await database.obtenerUsuario(idUsuario);
-		if(ctx.update.inline_query.chat_type === 'supergroup'){
+		if(control(ctx) === true){
 
 	
 		if(getUsuario){
@@ -63,6 +69,7 @@ function inlineCommands(bot,database){
 			tipoAlerta: ctx.update.chosen_inline_result.result_id,
 			alerta: ctx.update.chosen_inline_result.query
 		}
+		
 		if(await nuevoMensaje(datos,bot)){
 			console.log('Enviado')
 			try {
